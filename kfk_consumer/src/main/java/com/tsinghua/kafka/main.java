08ed57687e59c7e26269b8382c58440e732954dd
lenovo <zyr8@foxmail.com>
2021-04-19 16:39:49 +0800

    20210419 Producer Consumer connection

---
+++ kfk_consumer/src/main/java/com/tsinghua/kafka/main.java
@@ -0,0 +1,14 @@
+package com.tsinghua.kafka;
+
+public class main
+{
+    public static void main(String[] args)
+    {
+        MysqlConsumer MC = new MysqlConsumer("node1:9092");
+        MC.MysqlDBConnect("zhangyr");
+//        测试从kafka拉去数据并入库
+        MC.GetMessage("test");
+//        测试由自身插入一个tb_teacher的表格
+//        MC.Insert("tb_teacher");
+    }
+}
