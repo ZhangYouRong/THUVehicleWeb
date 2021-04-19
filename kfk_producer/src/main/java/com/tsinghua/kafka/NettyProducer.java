e4100a3d18e48ca643c3abbe1b567121239a8341
lenovo <zyr8@foxmail.com>
2021-04-19 16:37:53 +0800

    20210419 Producer Consumer connection

--- kfk_producer/src/main/java/com/tsinghua/kafka/NettyProducer.java
+++ kfk_producer/src/main/java/com/tsinghua/kafka/NettyProducer.java
@@ -24,10 +24,12 @@ public class NettyProducer
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         Producer<String, String> producer = new KafkaProducer<>(props);
 
+
+
         for (int i = 0; i < 100; i++)
         {
-            String data="id"+i;
-            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i),data));
+            String data= Integer.toString(i)+"'张有容','123456','男','教师','www.bbk.com','XX大学','2016-08-12 14:43:26','备注'";
+            producer.send(new ProducerRecord<String, String>("test", Integer.toString(i), data));
         }
         producer.close();
     }
